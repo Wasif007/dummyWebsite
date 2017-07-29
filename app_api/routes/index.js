3ee36697d255538a9adecadb23f2ec541adc5303
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-07-29 16:22:35 +0500

    routes debugging

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,10 @@ var ctrlInventory=require('../controllers/inventory');
 //Displaying All the inventories
 router.get('/displayInventory', ctrlInventory.inventoryList);
 //Adding new Inventories
-//router.post('/addInventory',ctrlInventory.inventoryAdd);
+router.post('/addInventory',ctrlInventory.inventoryAdd);
 //Deleting all the inventories
-//router.delete('/deleteInventory',ctrlInventory.inventoryDelete);
+router.delete('/deleteInventory',ctrlInventory.inventoryDelete);
 //pdating any Inventories 
-//router.update('/updateInventory',ctrlInventory.inventoryUpdate);
+router.put('/updateInventory',ctrlInventory.inventoryUpdate);
 
 module.exports = router;
