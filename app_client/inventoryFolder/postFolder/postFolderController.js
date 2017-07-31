498669efd9c348e98e72bf880c83f70ee5e3e5f8
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-07-31 16:38:26 +0500

    debugging

--- app_client/inventoryFolder/postFolder/postFolderController.js
+++ app_client/inventoryFolder/postFolder/postFolderController.js
@@ -9,47 +9,6 @@
   function postInventoryCtrl(postingInventory) {
     var vm = this;
 
-    vm.credentials = {
-      name : "",
-      quantity : "",
-      size : "",
-      colour:"",
-      article:"",
-      article_type:"",
-      price:""
-    };
-
-
-    vm.onSubmit = function () {
-      vm.formError = "";
-      if (!vm.credentials.name || !vm.credentials.quantity
-          || !vm.credentials.colour || !vm.credentials.size 
-          ||!vm.credentials.article || !vm.credentials.article_type
-          || !vm.credentials.price
-          )
-           {
-        vm.formError = "All fields required, please try again";
-        return false;
-     
-      } else {
-      	console.log("HELOOW WPR");
-        vm.doAddInventory();
-      }
-    };
-
-    vm.doAddInventory = function() {
-      vm.formError = "";
-   postingInventory
-        .assigningInventory(vm.credentials)
-        .error(function(err){
-         
-          vm.formError = err;
-        }
-        )
-        .then(function(){
-          $location.path('/');
-        });
-    };
 
   
 
