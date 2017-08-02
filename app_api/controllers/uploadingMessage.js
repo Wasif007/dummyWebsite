77252993791ea80d982c627b3154be93295c3c91
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-02 17:27:29 +0500

    sending mail in progress

--- app_api/controllers/uploadingMessage.js
+++ app_api/controllers/uploadingMessage.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-var nodemailer = require("nodemailer");
-
+var sendmail = require('sendmail')();
+ 
 var sendJSONresponse = function(res, status, content) {
   res.status(status);
   res.json(content);
@@ -9,32 +9,15 @@ var sendJSONresponse = function(res, status, content) {
 module.exports.sendingEmail = function(req, res) {
 
 
-// create reusable transport method (opens pool of SMTP connections)
-var smtpTransport = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
-    auth: {
-        user: "ar1363721@gmail.com",
-        pass: "wasifateeq0"
-    }
-});
 
-// setup e-mail data with unicode symbols
-var mailOptions = {
-   from: req.body.email, // sender address
+sendmail({
+    from: req.body.email, // sender address
     to: "ar1363721@gmail.com", // list of receivers
     subject: req.body.name, // Subject line
-    text: req.body.message // html body
-}
-
-// send mail with defined transport object
-smtpTransport.sendMail(mailOptions, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-
-    // if you don't want to use this transport object anymore, uncomment following line
-    //smtpTransport.close(); // shut down the connection pool, no more messages
+    text: req.body.message ,// html body,
+  }, function(err, reply) {
+    console.log(err && err.stack);
+    console.dir(reply);
 });
+
 };
