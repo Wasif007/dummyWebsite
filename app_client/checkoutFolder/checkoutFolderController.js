d037964a8a0af441bcd62ec3abcfe8ae2651f0a1
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-02 10:33:24 +0500

    template updating in progress

---
+++ app_client/checkoutFolder/checkoutFolderController.js
@@ -0,0 +1,12 @@
+(function () {
+
+  angular
+    .module('loc8rApp')
+    .controller('checkoutCtrl', checkoutCtrl);
+
+  checkoutCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
+  function checkoutCtrl ($scope, loc8rData, geolocation) {
+   
+  }
+
+})();
