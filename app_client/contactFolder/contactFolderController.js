8e43a7e23b00dd1a8c27a068ac7c67c5fb93d680
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-03 13:34:53 +0500

    debugging

--- app_client/contactFolder/contactFolderController.js
+++ app_client/contactFolder/contactFolderController.js
@@ -6,7 +6,23 @@
 
   contactCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
   function contactCtrl ($scope, loc8rData, geolocation) {
+    var vm=this;
+   vm.credentials={
+    name:"",
+    email:"",
+    message:""
+   }
    
+   vm.onSubmittion=function()
+   {
+      if(!vm.credentials.name || !vm.credentials.email 
+    || !vm.credentials.message)
+  {
+     vm.formError = "All fields required, please try again"
+         return false;
+     
+      }
+         }
   }
 
 })();
