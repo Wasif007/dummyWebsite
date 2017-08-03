2954293ee08c6b1b44cadcfee05451a1148c4383
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-03 17:41:54 +0500

    debugging

--- app_api/controllers/uploadingMessage.js
+++ app_api/controllers/uploadingMessage.js
@@ -11,14 +11,14 @@ module.exports.sendingEmail = function(req, res) {
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: req.body.email, // Your email id
+            user: 'ar1363721@gmail.com', // Your email id
             pass: 'wasifateeq0' // Your password
         }
     });
 
 var mailOptions = {
     from: req.body.email, // sender address
-    to: 'wasifateeq007@hotmail.com', // list of receivers
+    to: req.body.email, // list of receivers
     subject:req.body.name, // Subject line
     text: req.body.message //, // plaintext body
     // html: '<b>Hello world ✔</b>' // You can choose to send an HTML body instead
