b3235b52e76f5f3843229dd4f3e420b7390533b5
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-03 13:32:07 +0500

    debugging

--- app_client/contactFolder/contactFolderController.js
+++ app_client/contactFolder/contactFolderController.js
@@ -4,31 +4,9 @@
     .module('loc8rApp')
     .controller('contactCtrl', contactCtrl);
 
-  contactCtrl.$inject = ['$scope', 'loc8rData'];
-  function contactCtrl ($scope, loc8rData,) {
-   var vm=this;
-   vm.credentials={
-    name:"",
-    message:"",
-    email:""
-   }
+  contactCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
+  function contactCtrl ($scope, loc8rData, geolocation) {
    
-
-   vm.onSubmittion=function()
-   {
-   	
-      if(!vm.credentials.time || !vm.credentials.email 
-    || !vm.credentials.message )
- {
-     vm.formError = "All fields required, please try again"
-         return false;
- }    
-      }
-   
-
-
-
-
   }
 
 })();
