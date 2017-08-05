399996ca1cafba9e359fb7074c976a3589958e94
Wasif Ateeq <wasifateeq007@hotmail.com>
2017-08-05 12:27:49 +0500

    customizing pagse

--- app_client/contactFolder/contactFolderController.js
+++ app_client/contactFolder/contactFolderController.js
@@ -6,7 +6,28 @@
 
   contactCtrl.$inject = ['$scope', 'loc8rData', 'geolocation'];
   function contactCtrl ($scope, loc8rData, geolocation) {
-   
+   var vm = this;
+
+    vm.credentials = {
+      name : "",
+      email : "",
+      message:""
+    };
+
+
+   vm.onSubmit = function () {
+      vm.formError = "";
+      if (!vm.credentials.name || !vm.credentials.email
+          || !vm.credentials.message
+          )
+           {
+       console.log("Something is missing");
+        return false;
+     
+      } else {
+        console.log("Good to Go");
+      }
+    };
   }
 
 })();
